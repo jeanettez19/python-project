555333d83571c5ff30d4fa4ef969b7c467971f00
Tan Kok Kai <k10222863@gmail.com>
2025-01-27 10:15:49 +0800

    Edited QuickOverview.py and also Main.py to show the quick overview properly

--- QuickOverview.py
+++ QuickOverview.py
@@ -1,12 +1,20 @@
 import pandas as pd
 from datetime import datetime
+import sys
 
 class QuickOverview:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.data = pd.read_excel(file_path, sheet_name=['expense', 'budget'])
-        self.expense = self.data['expense']
-        self.budget = self.data['budget']
+    def __init__(self):
+        try:
+            self.expense = pd.read_excel("./data/expense.xlsx")
+            self.income = pd.read_excel("./data/income.xlsx")
+            self.transaction = pd.read_excel("./data/transaction.xlsx")
+        except PermissionError:
+            print("Permission denied to read the file, please close the file before proceeding.")
+            sys.exit(1)
+        except FileNotFoundError:
+            self.expense = pd.DataFrame(columns=["expenses_id", "date", "category", "description", "amount"])
+            self.income = pd.DataFrame(columns=["income_id", "date", "category", "description", "amount"])
+            self.transaction = pd.DataFrame(columns=["transaction_id", "date", "category", "description", "amount"])
 
     def get_current_month_overview(self):
         # Get the current month and year
@@ -14,11 +22,11 @@ class QuickOverview:
         current_year = datetime.now().year
 
         # Filter budget data for the current month and year
-        self.budget['date'] = pd.to_datetime(self.budget['date'], errors='coerce')
-        current_month_budget = self.budget[
-            (self.budget['date'].dt.month == current_month) & (self.budget['date'].dt.year == current_year)
+        self.income['date'] = pd.to_datetime(self.income['date'], errors='coerce')
+        current_month_income = self.income[
+            (self.income['date'].dt.month == current_month) & (self.income['date'].dt.year == current_year)
         ]
-        total_budget = current_month_budget['monthly_budget'].sum()
+        total_income = current_month_income['amount'].sum()
 
         # Filter expense data for the current month and year
         self.expense['date'] = pd.to_datetime(self.expense['date'], errors='coerce')
@@ -28,10 +36,10 @@ class QuickOverview:
         total_expense = current_month_expense['amount'].sum()
 
         # Calculate the remaining budget
-        remaining_budget = total_budget + total_expense  # Add because expenses are negative
+        #remaining_budget = total_budget + total_expense  # Add because expenses are negative
 
         return {
-            "Total Budget": total_budget,
+            "Total Income": total_income,
             "Total Expense": total_expense,
-            "Remaining Budget": remaining_budget
+           # "Remaining Budget": remaining_budget
         }
